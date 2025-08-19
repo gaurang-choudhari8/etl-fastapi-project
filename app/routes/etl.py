6875ff5bf8761b6e563fa8a20e60d74d4e334def
Gaurang Choudhari <gaurang.choudhari8@gmail.com>
2025-08-20 01:05:34 +0530

    adding depends

--- app/routes/etl.py
+++ app/routes/etl.py
@@ -1,15 +1,21 @@
 from typing import Annotated
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
+from services.s3_service import s3_service_class
 
 router = APIRouter()
 
 
 @router.post("/upload/")
-async def upload_file(file: UploadFile | None = None):
+async def upload_file(s3_client: Annotated[s3_service_class,Depends(s3_service_class)], file: UploadFile | None = None, key: str | None = None, bucket_name: str | None = None):
+    #If there is no file, then return error no file uploaded 
     if not file:
         return {"error": "No file uploaded"}
     
+    #If improper extension, then raise HTTP Error
     extn = file.filename.split(".")[-1].lower()
     if extn not in ["csv"]:
         raise HTTPException("Only CSV files are allowed")
+
+    #Call the service method to upload file
+    s3_client.upload_file(file, key, bucket_name)
 
--- app/services/s3_service.py
+++ app/services/s3_service.py
@@ -1,5 +1,7 @@
 import boto3
 from fastapi import UploadFile
+from botocore.exceptions import ClientError
+from boto3.exceptions import S3UploadFailedError
 class s3_service_class:
     def __init__(self, bucket_name: str):
         self.bucket_name = bucket_name
@@ -7,8 +9,29 @@ class s3_service_class:
         #self.__secret_key = secret_key
         self.s3_client=boto3.client('s3')
 
-    def upload_file(self, file: UploadFile, name: str):
-        pass
+    def upload_file(self, file: UploadFile, key: str, bucket_name: str):
+        try:
+            with file.file as data:
+                self.s3_client.upload_fileobj(data, bucket_name,key)
+            return {
+                "status": "Success",
+                "description": "The file upload succeeded"
+            }
+        except ClientError as ce:
+            return {
+                "error": "The bucket does not exist",
+                "description": str(ce)
+            }
+        except S3UploadFailedError as ufe:
+            return {
+                "error": "The upload failed",
+                "description": str(ufe)
+            }
+        except Exception as ue:
+            return {
+                "error": "Unknown exception",
+                "description": str(ue)
+            }
         
 
     def get_buckets(self, bucket_name: str):
@@ -16,4 +39,4 @@ class s3_service_class:
 
         bucketDict = response['Buckets']
         for bucket in bucketDict:
-
+            pass
